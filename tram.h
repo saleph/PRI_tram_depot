107db783daa54e23390030f9976673f9927a2964
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-11-06 22:28:42 +0100

    [ADD] TODO section

--- tram.h
+++ tram.h
@@ -1,6 +1,7 @@
 #ifndef TRAM_H_INCLUDED
 #define TRAM_H_INCLUDED
 
+/* TODO: in list's structure - add field RECORD_NO */
 typedef struct {
     int line_no;
     char *tram_type;
@@ -11,4 +12,4 @@ typedef struct {
 extern Tram* new_tram(int, char*, int, char*);
 extern char* new_string(char*);
 
-#endif // TRAM_H_INCLUDED
+#endif /* TRAM_H_INCLUDED */
